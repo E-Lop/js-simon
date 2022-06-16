a1758ba08ed6de87851a84ecad7b407e3b70a92d
E-Lop <edmondolopez@gmail.com>
2022-06-16 14:50:53 +0200

    analisi della consegna

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/* Visualizzare in un alert 5 numeri casuali.
+30 secondi dopo la chiusura dell'alert, l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati. */
+
+// ---------------------------------------------------------
+// ANALISI
+// ---------------------------------------------------------
+
+// generazione 5 numeri casuali univoci, salvati in array
+// prompt con 5 numeri da indovinare
+// creazione timer di 30 secondi
+// funzione con 5 prompt
+// salvataggio numeri utente in array
+// se numeri random include numero utente, push dentro terzo array
+// prompt con quanti numeri indovinati (length terzo array) e quali (console log terzo array)
